3085639fef236d503315a1922c8777f9c302109d
liupengfei <1608272694@qq.com>
2023-02-24 01:08:33 +0800

    fix: fix the bug of async component

--- src/components/async.tsx
+++ src/components/async.tsx
@@ -6,13 +6,15 @@ interface AsyncProps {
   fallback?: React.ReactNode
 }
 
+const LoaderMap = new WeakMap()
+
 export const Async = (props: AsyncProps) => {
-  const { loader, fallback } = props
-  if (typeof loader !== 'function') {
-    console.warn(loader)
-    return <div>出错了</div>
+  const { loader, fallback = null } = props
+  if (!LoaderMap.has(loader)) {
+    console.log(loader)
+    LoaderMap.set(loader, React.lazy(loader))
   }
-  const Component = React.lazy(loader)
+  const Component = LoaderMap.get(loader)
   return (
     <React.Suspense fallback={fallback}>
       <Component />
--- src/dev.ts
+++ src/dev.ts
@@ -1,15 +1,15 @@
 import type { Env } from '@rallie/block'
 import { core } from '~/blocks/core'
-import { loadHtml } from '@rallie/load-html'
+// import { loadHtml } from '@rallie/load-html'
 
 export const runInEntryMode = async (env: Env) => {
-  env.use(
-    loadHtml({
-      entries: {
-        'plugin-demo': 'http://localhost:4173',
-      },
-    }),
-  )
+  // env.use(
+  //   loadHtml({
+  //     entries: {
+  //       'plugin-demo': 'http://localhost:5173',
+  //     },
+  //   }),
+  // )
   core.setState('set root', (state) => {
     const container = document.createElement('div')
     document.body.appendChild(container)
